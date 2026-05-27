362d593f0d4c58a4ba5c58febf9d185aa1639351
ExtremeXT <extremext@lineageos.org>
2026-05-27 20:54:33 +0300

    [DNM] s5e9925: Setup ImsStack
    Change-Id: Ie459cb7df9c153738f9251c5eec742484a81f848
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

--- device-common.mk
+++ device-common.mk
@@ -135,7 +135,18 @@ $(call soong_config_set,lineage_health,fast_charge_value_none,1)
 
 # IMS
 PRODUCT_PACKAGES += \
-    CarrierConfigOverlayCommon
+    CarrierConfigOverlayCommon \
+    ImsStack \
+    Iwlan \
+    QualifiedNetworksService \
+    TelephonyOverlayCommon
+
+$(call inherit-product, packages/modules/ImsMedia/imsmedia.mk)
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/lineage/imsstack-carrier-config-ext
+
+$(call soong_config_set,imsstack_namespace,use_carrier_config_ext,true)
 
 # Init
 PRODUCT_PACKAGES += \
@@ -228,6 +239,7 @@ PRODUCT_PACKAGES += \
     android.hardware.sensor.stepcounter.prebuilt.xml \
     android.hardware.sensor.stepdetector.prebuilt.xml \
     android.hardware.telephony.gsm.prebuilt.xml \
+    android.hardware.telephony.ims.prebuilt.xml \
     android.hardware.telephony.satellite.prebuilt.xml \
     android.hardware.usb.accessory.prebuilt.xml \
     android.hardware.usb.host.prebuilt.xml \
