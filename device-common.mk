411834266a7d8a1a8d1bfce03e01f992b9fc064a
ExtremeXT <extremext@lineageos.org>
2026-08-17 22:14:20 +0300

    Revert "s5e9925: Switch to USBv2"
    This reverts commit be23a96f6de8daf07721cb80a20b592dad93186f.
    
    Change-Id: I13926aeb126f8959934f5cd9305218de0b1dd7fe
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

--- device-common.mk
+++ device-common.mk
@@ -57,7 +57,7 @@ $(call soong_config_set_bool,exynos_audio,use_offload_effect_library,true)
 $(call soong_config_set_bool,exynos_audio,use_quad_mic,true)
 $(call soong_config_set_bool,exynos_audio,use_sec_audio_samsungrecord,true)
 $(call soong_config_set_bool,exynos_audio,use_sec_audio_support_gamechat_spk_aec,true)
-$(call soong_config_set_bool,exynos_audio,use_usb_offload,false)
+$(call soong_config_set_bool,exynos_audio,use_usb_offload,true)
 $(call soong_config_set,exynos_audio,proxy_header,//$(LOCAL_PATH):audio_proxy_headers)
 $(call soong_config_set,exynos_audio,sec_resampler_library,//vendor/samsung/s5e9925-common:libSamsungPostProcessConvertor)
 
