2878cd307dc3b1c6be6d05e6a4ec36f63a724c87
ExtremeXT <extremext@lineageos.org>
2026-08-22 16:31:55 +0300

    s5e9925: Setup SoundBooster
    Change-Id: I8e64a999ccbc0a0ea68b28b560f280989ab2381b
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

--- device-common.mk
+++ device-common.mk
@@ -47,7 +47,9 @@ PRODUCT_PACKAGES += \
     audio.r_submix.default \
     audio.usbv2.default \
     bluetooth_with_le_audio_policy_configuration_7_0.xml \
-    SamsungDAP
+    libsamsungSoundbooster_plus \
+    SamsungDAP \
+    SoundBoosterStage
 
 TARGET_EXCLUDES_AUDIOFX := true
 
